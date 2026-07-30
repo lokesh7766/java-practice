3aafbe6dc0eafa50641b7063f13862354b1da2f7
lokesh <lokesh.77664@gmail.com>
2026-07-30 17:36:46 +0530

    hello

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 public class Main{
     public static void main(String[] args) {
-        System.out.println("hello lokesh G");
+  String name = "loki ";
+      System.out.println(" name = " + name);
+      System.out.println("hello lokesh G");
     }
 }
