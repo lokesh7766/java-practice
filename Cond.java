50c8e7674de076e36ea3e9b21eae44d9e16c0962
lokesh <lokesh.77664@gmail.com>
2026-07-30 18:25:14 +0530

    loops

--- Cond.java
+++ Cond.java
@@ -8,5 +8,39 @@ public class Cond {
         else{
             System.out.println("voting is not possible ");
         }
+
+        int time = 18;
+        String result = (time < 12)?"good moring ":"good evening";
+        System.out.println(result);
+          int day = 1;
+    switch(day)  
+{        case 1:
+        System.out.println("date 1");
+        break;
+        case 2:
+    System.out.println("Tuesday");
+    break;
+  case 3:
+    System.out.println("Wednesday");
+    break;
+  case 4:
+    System.out.println("Thursday");
+    break;
+    
+    
     }
+// int  i = 1;
+//     while (i < 100) {
+    
+//         System.out.println(i);
+//         i++;
+        
+//     }
+for( int i=0; i<10;i++){
+    System.out.println(i);
+}
+
+    }
+
+  
 }
