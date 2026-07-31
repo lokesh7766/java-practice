3ea6941a980c86df3b5ee1636b3cebf7d9cc5365
lokesh <lokesh.77664@gmail.com>
2026-07-31 07:51:42 +0530

    my first function

---
+++ met.java
@@ -0,0 +1,13 @@
+public class met {
+    
+    static void mymet(){
+        System.out.println(" method was called ");
+
+    }
+public static void main(String[] args){
+    mymet();
+    mymet();
+    mymet();
+    mymet();
+}
+}
