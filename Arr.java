a913fd8f7ae953e17353fa1e71cbda635baafdce
lokesh <lokesh.77664@gmail.com>
2026-07-31 07:11:02 +0530

    arrays

---
+++ Arr.java
@@ -0,0 +1,16 @@
+public class Arr{
+     public static void main(String[] args){
+String[] cars = {"honda","bolero " , "fortuner"};
+int[] nums = new int[6];
+nums[1] = 77;
+System.out.println(cars);
+System.out.println(nums[1]);
+int[] numbers  = {10,20,30,29,12};
+for(int i =0;i < numbers.length;i++){
+     System.out.println(numbers[i]);
+     i++;
+}
+
+     }
+    
+}
